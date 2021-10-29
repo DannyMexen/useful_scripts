9731aeb7afdf08bd40eae2f7340d0ddda969a638
Daniel Mwaba Mwale <dannymexen@gmail.com>
2021-10-29 09:58:49 +0200

    Update web_project.zsh

---
+++ web_project.zsh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+# WEB_PROJECTS
+# Create a generic, simple directory for framework-less web projects
+# - styles
+#	- css
+#	- fonts
+# - images
+# index.html
+# README.md
+
+# Danny Mexen
+# danny@arcariusmexen.com
+# 2021-10-29
+# Version 1.0.0
+
+# Steps
+# 1. Prompt for project name
+# 2. Create root directory with project name
+# 3. Open/navigate to root directory
+# 4. Create `styles` directory
+# 5. Create `images` directory
+# 6. Create index.html file
+# 7. Create README.md
+# 8. Open `styles` directory
+# 9. Create `css` directory
+# 10. Create `fonts` directory
+# 11. Open root directory
+# 12. Initiliaze with git
